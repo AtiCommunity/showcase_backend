709d053b0196cd4a30ce6bbb629943eccbbaec59
Antoine ROBIN <antoine.robin@aticommunity.fr>
2024-08-19 03:26:56 +0200

    feat(users): add user route with crud

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -3,33 +3,75 @@ const bcrypt = require('bcrypt')
 const User = require('../models/user.models')
 
 exports.register = async (req, res) => {
-    var newUser = new User({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 10),
-    })
     try {
+        const newUser = new User({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10),
+        })
         await newUser.save()
+        return res.status(200).json(newUser)
     } catch (error) {
-        console.log(error.message)
-        if (error.name === "ValidationError") {
-            let errors = {};
-
-            Object.keys(error.errors).forEach((key) => {
-                errors[key] = error.errors[key].message;
-            });
-
-            return res.status(400).json(errors);
-        }
-        if (error.name === "MongoServerError") {
-            return res.status(400).json({ message: error.message });
-        }
-        return res.status(500).json({ message: "Something went wrong!" });
+        console.log(error)
+        return res.status(400).json({ message: error.message });
     }
-    return res.status(200).json(newUser)
 }
 
 exports.login = (req, res) => {
     res.send("OK")
+}
+
+exports.getUsers = async (req, res) => {
+    try {
+        if (!req.params.id) {
+            return res.status(200).json(await User.find())
+        }
+        const getUser = await User.findOne({ _id: req.params.id })
+        if (getUser == null) {
+            return res.status(404).json({ error: `user ${req.params.id} not found` })
+        }
+        return res.status(200).json(getUser)
+    }
+    catch (error) {
+        console.log(error)
+        return res.status(400).json({ error: error.message })
+    }
+}
+
+exports.putUsers = async (req, res) => {
+    try {
+        var getUser = await User.findOne({ _id: req.params.id })
+        if (getUser == null) {
+            return res.status(404).json({ error: `user ${req.params.id} not found` })
+        }
+        if (req.body == 0) {
+            return res.status(400).json({ error: `no request body` })
+        }
+        if (req.body.password != null) {
+            req.body.password = bcrypt.hashSync(req.body.password, 10)
+        }
+        await User.updateOne(getUser, req.body)
+        getUser = await User.findOne({ _id: req.params.id })
+        return res.status(200).json(getUser)
+    }
+    catch (error) {
+        console.log(error)
+        return res.status(400).json({ error: error.message })
+    }
+}
+
+exports.deleteUsers = async (req, res) => {
+    try {
+        const getUser = await User.findOne({ _id: req.params.id })
+        if (getUser == null) {
+            return res.status(404).json({ error: `user ${req.params.id} not found` })
+        }
+        await User.deleteOne(getUser)
+        return res.status(200).json(getUser)
+    }
+    catch (error) {
+        console.log(error)
+        return res.status(400).json({ error: error.message })
+    }
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -7,4 +7,9 @@ const authController = require("../controllers/auth.controllers")
 router.post("/register", authController.register)
 router.post("/login", authController.login)
 
+router.get("/users", authController.getUsers)
+router.get("/users/:id", authController.getUsers)
+router.put("/users/:id", authController.putUsers)
+router.delete("/users/:id", authController.deleteUsers)
+
 module.exports = router
